25a2f1a78bc2c6f1fbc372a6c363e82829dd11dd
cnktrhn <145922656+cnktrhn@users.noreply.github.com>
2026-02-17 14:42:24 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,7 @@
 self.addEventListener("install", event => {
-  console.log("Service Worker Installed");
+  self.skipWaiting();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(fetch(event.request));
 });
